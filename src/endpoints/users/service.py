9d3f00fa94ac1819e0704ffcdcbe5a16e126de2f
Gabriel Frigo <gfrigo.sena@gmail.com>
2026-03-23 16:51:11 -0300

    fix: removing passlib, using bcrypt

--- src/endpoints/users/service.py
+++ src/endpoints/users/service.py
@@ -1,11 +1,9 @@
+import bcrypt
 from sqlalchemy.orm import Session
-from passlib.context import CryptContext
 
 from src.endpoints.users.model import User
 from src.endpoints.users.schema import UserCreate
 from src.endpoints.users import repository
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def create_user(db:Session, data:UserCreate) -> User:
   existing_user = repository.get_user_by_email(db, data.email)
@@ -13,7 +11,10 @@ def create_user(db:Session, data:UserCreate) -> User:
   if existing_user:
     raise ValueError("Email já cadastrado.")
 
-  hashed_password = pwd_context.hash(data.password)
+  hashed_password = bcrypt.hashpw(
+    data.password.encode("utf-8"),
+    bcrypt.gensalt()
+  ).decode("utf-8")
   
   user = repository.create_user(db, data, hashed_password)
   return user
