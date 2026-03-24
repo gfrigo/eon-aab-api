5e5e006d372b71df7b88027222b75a01cd9d9be6
Gabriel Frigo <gfrigo.sena@gmail.com>
2026-03-23 22:23:55 -0300

    fix: users table name

--- src/endpoints/users/model.py
+++ src/endpoints/users/model.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.core.database import Base
 
 class User(Base):
-  __tablename__ = "ABS_Users"
+  __tablename__ = "Users"
 
   id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
   email: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
